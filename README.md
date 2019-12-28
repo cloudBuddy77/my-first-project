76cbf948deda675d78683f716ae68c10ce2e2073
Cloudbuddy <cloudbuddy9@gmail.com>
2019-12-29 00:21:53 +0530

    Updated code and readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Meteor Landing Sites
 This is my first project. It will find meteor landing sites.
+
+## Running
+
+This pachange requires Python 3 and the Requests package.
+
+`python3 find-meteors-near-me.py`
--- meteors-landing-app/find-meteors-near-me.py
+++ meteors-landing-app/find-meteors-near-me.py
@@ -1,9 +1,5 @@
 import requests, math
 
-meteor_resp=requests.get("https://data.nasa.gov/resource/gh4g-9sfh.json")
-meteor_resp_json=meteor_resp.json()
-
-
 def calc_dist(lat1, lon1, lat2, lon2):
     lat1 = math.radians(lat1)
     lon1 = math.radians(lon1)
@@ -17,17 +13,19 @@ def calc_dist(lat1, lon1, lat2, lon2):
 
     return 6372.8 * 2 * math.asin(math.sqrt(h))
 
+def get_dist(meteor):
+    return meteor.get('dist', math.inf)
+
 my_loc = (18.520760, 73.855408)
 
+meteor_resp=requests.get("https://data.nasa.gov/resource/gh4g-9sfh.json")
+meteor_resp_json=meteor_resp.json()
 
 for meteor in meteor_resp_json:
     if not ('reclat' in meteor and 'reclong' in meteor):
         continue
     meteor['dist']=calc_dist(float(meteor['reclat']), float(meteor['reclong']), my_loc[0], my_loc[1])
 
-
-def get_dist(meteor):
-    return meteor.get('dist', math.inf)
 
 meteor_resp_json.sort(key=get_dist)
 
